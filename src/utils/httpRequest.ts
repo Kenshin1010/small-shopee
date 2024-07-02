c08344d9613baf220cefcca76ebee304feed0d30
anh.do <tuananh9h@gmail.com>
2024-07-02 16:59:33 +0700

    Change httpRequest

--- src/utils/httpRequest.ts
+++ src/utils/httpRequest.ts
@@ -1,8 +1,8 @@
 import axios, { AxiosInstance } from "axios";
 
 const httpRequest: AxiosInstance = axios.create({
-  baseURL: `http://localhost:5555/`,
-  // baseURL: `https://small-shopee-node-express.vercel.app/`,
+  // baseURL: `http://localhost:5555/`,
+  baseURL: `https://small-shopee-node-express.vercel.app/`,
 });
 
 export default httpRequest;
